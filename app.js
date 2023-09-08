0a26d39abbc182d3b85d990af00bfc69f3fbea57
unknown <harisshahbaz707@gmail.com>
2023-09-08 15:46:11 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,111 @@
+// Chapter No 5
+
+
+//  Question no 1
+// var num1 = 3;
+// var num2 = 5;
+// document.write ("Sum of 3 and 5 is ");
+// document.write (num1 + num2);
+
+
+
+// Question no 2
+// var num1 = 3;
+// var num2 = 5;
+// document.write ("Sum of 3 and 5 is ");
+// document.write (num1 * num2);
+
+// var num1 = 3;
+// var num2 = 5;
+// document.write ("Sum of 3 and 5 is ");
+// document.write (num1 / num2);
+
+// var num1 = 3;
+// var num2 = 5;
+// document.write ("Sum of 3 and 5 is ");
+// document.write (num1 - num2);
+
+// var num1 = 3;
+// var num2 = 5;
+// document.write ("Sum of 3 and 5 is ");
+// document.write (num1 % num2);
+
+
+
+// Question no 4
+// var price = 600;
+// document.write ("Total cost to buy 5 tickets to a movie is ");
+// document.write (price * 5);
+// document.write ("PKR");
+
+
+
+// Question no 5
+// var num = 4;
+// var result;
+// for (var i = 1; i <= 10; i++) {
+//     document.write(num + '*' + i + '=' + num * i + '\n')
+// }
+
+
+
+// Question no 6
+// document.write (F = (C * 32) * 5/9);
+
+
+
+// Question no 7
+// var price1 = 650;
+// document.write ("Price of item 1 is 650 ");
+// var quantity1 = 3;
+// document.write (" Quantity of item 1 is 3 ");
+// var price2 = 100;
+// var result = price1 * quantity1;
+// document.write (" Price of item 2 is 100 ");
+// var quantity2 = 7;
+// document.write (" Quantity of item 2 is 7 ");
+// var result2 = price2 * quantity2;
+// var shipping = 100;
+// document.write (" Shipping charges 100");
+// document.write (" Total cost of your order is " + (result + result2 + shipping));
+
+
+
+// Question no 8
+// var obtained = 804;
+// var total = 980;
+// var percentage = obtained/total*100;
+// document.write (percentage + "%");
+
+
+
+// Question no 9
+// var dollar = 104.80;
+// var rayal = 28;
+// document.write ("Total pakistani rupess " +((10 * dollar) + (25 * rayal)));
+
+
+
+// Question no 10
+// document.write ((5 * 10) / 2);
+
+
+
+// Question no 11
+// var currentYear = prompt("Enter your current Year: ");
+// var birthYear = +prompt("Enter your Birth Year: ");
+//  if (currentYear === 20 || birthYear === 2003) {
+//        document.write ("you are 20 year old");
+//  }
+//  else {
+//     document.write ("you are not 20 year old");
+//  }
+
+
+
+// Question no 12
+// var snack = prompt ("Enter your favorite snack");
+// var age = prompt ("Enter your current Age");
+// var maximumAge = prompt("Enter When you will die");
+// var amountofsnacks = prompt("Enter Amount of snacks per day");
+// document.write ((maximumAge - age) * amountofsnacks * 365);
